d09d555d14ffe1494da219a1921a621c2a32c782
Aman Gupta <68396428+aman97703@users.noreply.github.com>
2023-05-09 19:19:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Playlist from "./Components/Playlist";
+import { useQuery, gql } from "@apollo/client";
+import SongMain from "./Components/SongMain";
 
-function App() {
+const PLAYLIST_QUERY = gql`
+  {
+    getPlaylists {
+      id
+      title
+    }
+  }
+`;
+
+const App = () => {
+  const [currentPlaylist, setCurrentPlaylist] = useState("");
+  const [playlistArr, setPlayListArr] = useState([]);
+  const { data, loading } = useQuery(PLAYLIST_QUERY);
+
+  useEffect(() => {
+    if (data) {
+      setPlayListArr(data.getPlaylists);
+      setCurrentPlaylist(data.getPlaylists[0]);
+    }
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="main">
+        <Playlist
+          playlistArr={playlistArr}
+          currentPlaylist={currentPlaylist}
+          setCurrentPlaylist={setCurrentPlaylist}
+          loading={loading}
+        />
+        <SongMain
+          currentPlaylist={currentPlaylist}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
